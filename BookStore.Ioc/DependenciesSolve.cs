405998f73813c4d47714d15f84189a0a3f39ef26
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-16 14:53:03 -0300

    RepositoryBase

---
+++ BookStore.Ioc/DependenciesSolve.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.Domain.Interfaces.Repositories.Base;
+using BookStore.Infra.Persistence;
+using BookStore.Infra.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookStore.Ioc;
+
+public static class DependenciesSolve
+{
+    public static IServiceCollection SolveDepencies(this IServiceCollection ser)
+    {
+        ser.AddScoped<BookContext>();
+        // ser.AddTransient<IUnitOfWork, UnitOfWork>();
+
+        // add repositories
+        ser.AddTransient(typeof(IRepositoryBase<,>), typeof(RepositoryBase<,>));
+
+        // ser.AddTransient<IPostRepository, PostRepository>();
+
+        //add services
+        // ser.AddTransient<IPostService, PostService>();
+        //ser.AddTransient<IReactionService, ReactionService>();
+
+        return ser;
+    }
+}
