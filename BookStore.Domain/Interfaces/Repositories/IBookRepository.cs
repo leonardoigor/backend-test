a8d5c879fcec7de4c77a1d07c903ca34039bbd15
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-16 11:44:32 -0300

    IBookRepository

---
+++ BookStore.Domain/Interfaces/Repositories/IBookRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Domain.Entities;
+using BookStore.Domain.Interfaces.Repositories.Base;
+
+namespace BookStore.Domain.Interfaces.Repositories;
+
+public interface IBookRepository : IRepositoryBase<BookEntity, Guid>
+
+{
+}
