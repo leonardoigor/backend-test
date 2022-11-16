a7b9e9817ace864d589e1c2c378eb426d1aeefca
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-16 09:56:48 -0300

    entity base and entity book

---
+++ BookStore.Domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using prmToolkit.NotificationPattern;
+
+namespace BookStore.Domain.Entities.Base;
+
+public class EntityBase : Notifiable
+{
+    public EntityBase()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; set; }
+}
---
+++ BookStore.Domain/Entities/BookEntity.cs
@@ -0,0 +1,7 @@
+﻿using BookStore.Domain.Entities.Base;
+
+namespace BookStore.Domain.Entities;
+
+public class BookEntity : EntityBase
+{
+}
