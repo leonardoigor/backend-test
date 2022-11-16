3491d47369d3a5533a0a93b978e80bc14d3d7c94
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-16 11:07:06 -0300

    BookEntity

--- BookStore.Domain/Entities/BookEntity.cs
+++ BookStore.Domain/Entities/BookEntity.cs
@@ -4,4 +4,14 @@ namespace BookStore.Domain.Entities;
 
 public class BookEntity : EntityBase
 {
+    public string Title { get; set; }
+    public string Isbn { get; set; }
+    public long PageCount { get; set; }
+    public string PublishedDate { get; set; }
+    public Uri ThumbnailUrl { get; set; }
+    public string ShortDescription { get; set; }
+    public string LongDescription { get; set; }
+    public string Status { get; set; }
+    public string Authors { get; set; }
+    public string Categories { get; set; }
 }
