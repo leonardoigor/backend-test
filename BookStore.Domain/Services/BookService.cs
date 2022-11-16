4ccdde5077c6b93aa6368fa342c18a0e9b8421fd
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-16 12:07:21 -0300

    BookService

---
+++ BookStore.Domain/Services/BookService.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Domain.Interfaces.Repositories;
+using BookStore.Domain.Interfaces.Services;
+using prmToolkit.NotificationPattern;
+
+namespace BookStore.Domain.Services;
+
+public class BookService:Notifiable,IBookService
+{
+    private readonly IBookRepository _bookRepository;
+    public BookService(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+}
--- BookStore.Tests/BooksTests/BookAddTest.cs
+++ BookStore.Tests/BooksTests/BookAddTest.cs
@@ -1,10 +1,19 @@
+using BookStore.Domain.Interfaces.Repositories;
+using BookStore.Domain.Interfaces.Services;
+using BookStore.Domain.Services;
+using Moq;
+
 namespace BookStore.Tests.BooksTests;
 
 public class BookAddTest
 {
+    IBookService _bookService;
+    IBookRepository _bookRepository;
     [SetUp]
     public void Setup()
     {
+        _bookRepository=Mock.Of<IBookRepository>();
+        _bookService = new BookService(_bookRepository);
     }
 
     [Test]
